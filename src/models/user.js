ab7412255b79876a7083d9c7e13254b4ae181350
Ali Shah <s.ashah@outlook.com>
2021-03-11 11:29:58 -0500

    [Update] cleaned up pre-save hook

--- src/models/user.js
+++ src/models/user.js
@@ -1,36 +1,29 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  username: { type: String, required: true },
-  password: { type: String, select: false },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
   locations: [{ type: Schema.Types.ObjectId, ref: 'Location' }]
 }, { timestamps: true });
 
-UserSchema.pre('save', function generate(next) {
-  if (!this.isModified('password')) return next();
-  bcrypt.genSalt(10)
-    .then((salt) => bcrypt.hash(this.password, salt))
-    .then((hash) => {
-      this.password = hash;
-    })
-    .catch((err) => {
-      throw err;
-    });
-  return next();
-});
+UserSchema.plugin(uniqueValidator);
 
-UserSchema.methods.comparePassword = function compare(password, done) {
-  bcrypt.compare(password, this.password)
-    .then((result) => {
-      done(result);
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
+// eslint-disable-next-line func-names
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    next();
+  } else {
+    bcrypt.genSalt(10)
+      .then((salt) => bcrypt.hash(this.password, salt))
+      .then((hash) => { this.password = hash; })
+      .catch((err) => { throw err; });
+  }
+  next();
+});
 
 const model = mongoose.model('User', UserSchema);
 

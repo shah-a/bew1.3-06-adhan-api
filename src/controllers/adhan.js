c1fe5022c08938a4dbc5196a2a9416f6943e6718
Ali Shah <s.ashah@outlook.com>
2021-03-18 01:12:01 -0400

    [Added] timezone display
    Notes:
    - I don't think there's a way to know the user's timezone without a
      a frontend/client-side. So the server will display all adhan times
      in whatever timezone the server is running on.
    - The term "display_timezone" means the timezone of the times displayed
      on screen -- not the timezone of the location being queried.

--- src/controllers/adhan.js
+++ src/controllers/adhan.js
@@ -6,14 +6,15 @@ const getAll = (req, res) => {
   const { year, month, day } = req.query;
   const outputs = {};
 
-  Location.find({ user: req.user._id })
+  Location.find({ user: req.user._id }).lean()
     .then((queries) => {
       if (queries.length > 0) {
         queries.forEach((query, i) => {
-          const location = query.name;
+          const locId = query._id;
+          const locName = query.name;
           const { lat, long } = query;
           // eslint-disable-next-line object-curly-newline
-          outputs[`location_${i + 1}`] = adhan({ prayer, location, lat, long, year, month, day });
+          outputs[`location_${i + 1}`] = adhan({ prayer, locId, locName, lat, long, year, month, day });
         });
         return res.json(outputs);
       }
@@ -28,13 +29,14 @@ const getOne = (req, res) => {
   const { prayer } = req.query;
   const { year, month, day } = req.query;
 
-  Location.findOne({ _id: req.params.locationId, user: req.user._id })
+  Location.findOne({ _id: req.params.locationId, user: req.user._id }).lean()
     .then((query) => {
       if (query) {
-        const location = query.name;
+        const locId = query._id;
+        const locName = query.name;
         const { lat, long } = query;
         // eslint-disable-next-line object-curly-newline
-        return res.json(adhan({ prayer, location, lat, long, year, month, day }));
+        return res.json(adhan({ prayer, locId, locName, lat, long, year, month, day }));
       }
       return res.status(404).json({ message: 'No location found.' });
     })
--- src/utils/adhan.js
+++ src/utils/adhan.js
@@ -2,9 +2,10 @@ const { PrayerTimes, Coordinates, CalculationMethod } = require('adhan');
 
 const adhanTimes = (context) => {
   const { prayer } = context;
-  const { location } = context;
+  const { locId, locName } = context;
   const { lat, long } = context;
   const { year, month, day } = context;
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
   const coords = new Coordinates(lat, long);
   const date = (year && month && day) ? new Date(year, parseInt(month, 10) - 1, day) : new Date();
@@ -14,8 +15,10 @@ const adhanTimes = (context) => {
 
   if (['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'].includes(prayer)) {
     return {
-      location,
+      location_id: locId,
+      location_name: locName,
       date: adhan.date.toLocaleDateString(),
+      display_timezone: timezone,
       prayer_time: {
         [prayer]: adhan[prayer].toLocaleTimeString()
       }
@@ -23,8 +26,10 @@ const adhanTimes = (context) => {
   }
 
   const result = {
-    location,
+    location_id: locId,
+    location_name: locName,
     date: adhan.date.toLocaleDateString(),
+    display_timezone: timezone,
     sunrise: adhan.sunrise.toLocaleTimeString(),
     prayer_times: {
       fajr: adhan.fajr.toLocaleTimeString(),
